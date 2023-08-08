837891f00c3d4d23a645e35740daf184a5b89494
Marcelo de Abreu <marcelodeabreu@Marcelos-MacBook-Pro.local>
2023-08-08 16:01:59 -0300

    commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Marcelo de Abreu on 08/08/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
